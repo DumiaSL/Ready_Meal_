09341076976e320954b887c5ac66ff7a5a1c3082
Dumidu Fernando <dumidufarnando@gmail.com>
2023-03-29 07:42:21 +0530

    designing splash screen

---
+++ app/src/main/java/com/example/meal_preparation_application/Splash_screen.kt
@@ -0,0 +1,11 @@
+package com.example.meal_preparation_application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
